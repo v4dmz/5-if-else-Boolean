36c90d3f561b93cd4aee4961e140052a5cdec5f5
v4dmz <143401420+v4dmz@users.noreply.github.com>
2024-03-21 03:49:28 -0700

    5 if-else-Boolean

---
+++ 5 if-else-Boolean/chp5/app.js
@@ -0,0 +1,45 @@
+// //Boolean
+// // // console.log(typeof(5<8));
+// // // console.log(typeof'true');
+// // console.log(5==5.0);
+// // console.log(5==4);
+
+
+// // Equality
+// // console.log(5==5.0);
+// // console.log(5==5);
+
+
+
+// // console.log(5===5.0);
+// // console.log(5===5);
+
+
+
+
+// console.log(5=='5.0');
+// console.log(5=='5');
+
+
+
+// console.log(5==='5.0');
+// console.log(5==='5');
+
+
+console.log("checking if you can drive");
+let age = 70;
+//or
+if(age > 65) {
+    console.log("Please ask someone else to drive");
+//or
+} else if(age > 18) {
+    console.log("you can drive ")
+} else {
+    console.log("use a Bicycle");
+
+}
+
+
+// if(age > 18) console.log("you can dirve"); else 
+//  console.log("use a Bicycle");
+
